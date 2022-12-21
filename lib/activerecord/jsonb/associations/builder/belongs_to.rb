8291fca724318029938a0bb7fc0e1406d5a20f59
Aleksandr Grigorev <vill05@mail.ru>
2022-12-21 09:47:43 +0000

    Add `add_association_accessor_methods` method

--- lib/activerecord/jsonb/associations/builder/belongs_to.rb
+++ lib/activerecord/jsonb/associations/builder/belongs_to.rb
@@ -6,6 +6,48 @@ module ActiveRecord
           def valid_options(options)
             super + [:store]
           end
+
+          def add_association_accessor_methods(mixin, reflection)
+            foreign_key = reflection.foreign_key.to_s
+
+            mixin.class_eval <<-CODE, __FILE__, __LINE__ + 1
+              @@jsonb_foreign_key_store ||= {}
+
+              @@jsonb_foreign_key_store[foreign_key] = reflection.options[:store]
+
+              def #{foreign_key}=(value)
+                public_send(self.class.jsonb_foreign_key_store['#{foreign_key}'])['#{foreign_key}'] = value
+              end
+
+              if @@jsonb_foreign_key_store.size < 2
+                class << self
+                  def jsonb_foreign_key_store
+                    @@jsonb_foreign_key_store
+                  end
+                end
+
+                def _read_attribute(attr_name)
+                  key = attr_name.to_s
+
+                  if self.class.jsonb_foreign_key_store.keys.include?(key)
+                    public_send(self.class.jsonb_foreign_key_store[key])[key]
+                  else
+                    super
+                  end
+                end
+
+                def []=(key, value)
+                  key = key.to_s
+
+                  if self.class.jsonb_foreign_key_store.keys.include?(key)
+                    public_send(self.class.jsonb_foreign_key_store[key])[key] = value
+                  else
+                    super
+                  end
+                end
+              end
+            CODE
+          end
         end
       end
     end
